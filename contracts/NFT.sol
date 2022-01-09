bea695c57531c0c321e0369e5ff0d8ef00ff5216
Michael Cohen <michael.cohen@opensea.io>
2022-01-09 16:41:10 -0500

    Adding metadata to an NFT contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,18 +5,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private currentTokenId;
-    
-    constructor() ERC721("NFTTutorial", "NFT") {}
-    
-    function mintTo(address recipient)
-        public
-        returns (uint256)
-    {
-        currentTokenId.increment();
-        uint256 newItemId = currentTokenId.current();
-        _safeMint(recipient, newItemId);
-        return newItemId;
-    }
+  using Counters for Counters.Counter;
+
+  Counters.Counter private currentTokenId;
+
+  /// @dev Base token URI used as a prefix by tokenURI().
+  string public baseTokenURI;
+
+  constructor() ERC721("NFTTutorial", "NFT") {
+    baseTokenURI = "";
+  }
+
+  function mintTo(address recipient) public returns (uint256) {
+    currentTokenId.increment();
+    uint256 newItemId = currentTokenId.current();
+    _safeMint(recipient, newItemId);
+    return newItemId;
+  }
+
+  /// @dev Returns an URI for a given token ID
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseTokenURI;
+  }
+
+  /// @dev Sets the base token URI prefix.
+  function setBaseTokenURI(string memory _baseTokenURI) public {
+    baseTokenURI = _baseTokenURI;
+  }
 }
--- scripts/mint.js
+++ scripts/mint.js
@@ -1,5 +1,6 @@
 const { task } = require("hardhat/config");
 const { getContract } = require("./helpers");
+const fetch = require("node-fetch");
 
 task("mint", "Mints from the NFT contract")
 .addParam("address", "The address to receive a token")
@@ -9,4 +10,30 @@ task("mint", "Mints from the NFT contract")
 		gasLimit: 500_000,
 	});
 	console.log(`Transaction Hash: ${transactionResponse.hash}`);
+});
+
+task("set-base-token-uri", "Sets the base token URI for the deployed smart contract")
+.addParam("baseUrl", "The base of the tokenURI endpoint to set")
+.setAction(async function (taskArguments, hre) {
+	const contract = await getContract("NFT", hre);
+	const transactionResponse = await contract.setBaseTokenURI(taskArguments.baseUrl, {
+		gasLimit: 500_000,
+	});
+	console.log(`Transaction Hash: ${transactionResponse.hash}`);
+});
+
+
+task("token-uri", "Fetches the token metadata for the given token ID")
+.addParam("tokenId", "The tokenID to fetch metadata for")
+.setAction(async function (taskArguments, hre) {
+	const contract = await getContract("NFT", hre);
+	const response = await contract.tokenURI(taskArguments.tokenId, {
+		gasLimit: 500_000,
+	});
+	
+	const metadata_url = response;
+	console.log(`Metadata URL: ${metadata_url}`);
+
+	const metadata = await fetch(metadata_url).then(res => res.json());
+	console.log(`Metadata fetch response: ${JSON.stringify(metadata, null, 2)}`);
 });
