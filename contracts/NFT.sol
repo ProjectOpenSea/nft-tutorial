679c9fb9dbc978dd6872dd195ed39c1e735a3731
Michael Cohen <michael.cohen@opensea.io>
2022-01-09 18:49:52 -0500

    Etherscan verification, payment, and ownership

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721 {
+
+contract NFT is ERC721, PullPayment, Ownable {
   using Counters for Counters.Counter;
+
+  // Constants
+  uint256 public constant TOTAL_SUPPLY = 10_000;
+  uint256 public constant MINT_PRICE = 0.08 ether;
 
   Counters.Counter private currentTokenId;
 
@@ -16,7 +23,11 @@ contract NFT is ERC721 {
     baseTokenURI = "";
   }
 
-  function mintTo(address recipient) public returns (uint256) {
+  function mintTo(address recipient) public payable returns (uint256) {
+    uint256 tokenId = currentTokenId.current();
+    require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+    require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
+
     currentTokenId.increment();
     uint256 newItemId = currentTokenId.current();
     _safeMint(recipient, newItemId);
@@ -29,7 +40,12 @@ contract NFT is ERC721 {
   }
 
   /// @dev Sets the base token URI prefix.
-  function setBaseTokenURI(string memory _baseTokenURI) public {
+  function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
     baseTokenURI = _baseTokenURI;
+  }
+
+  /// @dev Overridden in order to make it an onlyOwner function
+  function withdrawPayments(address payable payee) public override onlyOwner virtual {
+      super.withdrawPayments(payee);
   }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,9 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("./scripts/deploy.js");
 require("./scripts/mint.js");
+require("@nomiclabs/hardhat-etherscan");
 
-const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.0",
@@ -23,5 +24,8 @@ module.exports = {
       url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 }
